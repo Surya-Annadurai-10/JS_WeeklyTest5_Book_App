608eb01b42835bad3b5a700a01f916b0635fa4dd
Surya Annadurai <suryaannadurai1041999@gmail.com>
2024-11-15 22:38:05 +0530

    updated

--- login/login.js
+++ login/login.js
@@ -18,38 +18,58 @@ function loginValidation(){
     return false;
   }
     let Emailresult = localUserarr.some((value) =>{
-      return  value.email == email.value
+       if( value.email == email.value){
+       if(value.password == password.value){
+        console.log("true");
+        return true;
+
+       }else{
+      
+        console.log("false")
+        password.style.boxShadow = "0 0 20px rgb(255, 0 , 0)"
+         alert("Enter Correct Password");
+        return false;
+      
+       }
+      
+      
+      }
  
     });
 
-    let passwordResult = localUserarr.some((value) =>{
-        return  value.password == password.value
+    // console.log()
+
+    if(!Emailresult){
+     
+       email.style.boxShadow = "0 0 20px rgb(255, 0 , 0)"
+       alert("Enter Correct email");
+       return false;
+  }else{
+    return true;
+  }
+
+    // let passwordResult = localUserarr.some((value) =>{
+    //     return  value.password == password.value
    
-      });
+    //   });
 
-      console.log(passwordResult)
+      // console.log(passwordResult)
        
-console.log(Emailresult, passwordResult);
+// console.log(Emailresult, passwordResult);
 
-    if (Emailresult && passwordResult){
-        return true;
-    }else{
-        if(Emailresult){
-            // alert("Enter Correct Password");
-             password.style.boxShadow = "0 0 20px rgb(255, 0 , 0)"
-            return false;
-        }
+    // if (Emailresult && passwordResult){
+    //     return true;
+    // }else{
+        
 
-        if(passwordResult){
-            // alert("Enter Correct Email");
-            email.style.boxShadow = "0 0 20px rgb(255, 0 , 0)"
-            return false
+    //     if(passwordResult){
+           
             
-        }
-    }
+    //     }
+    // }
 
   
-   
+   return false;
 }
 
 
@@ -60,7 +80,7 @@ let header = document.querySelector("header");
 let sign1 = document.querySelector(".sign1")
 let sign2 = document.querySelector(".sign2")
 
-let signUpBtn = document.querySelector(".sign-up")
+// let signUpBtn = document.querySelector(".sign-up")
 
 let formCon = document.querySelector(".form-container")
 let login = document.querySelector(".login")
@@ -77,7 +97,7 @@ darkBall.addEventListener("click", () => {
     header.style.color = "#fff";
     formCon.style.backgroundColor = "#000";
     formCon.style.color = "#ffff";
-    signUpBtn.style.backgroundColor = "#ff0000"
+    signBtn.style.backgroundColor = "#ff0000"
 sign1.style.color = "#fff"
 sign2.style.color = "#fff"
 
@@ -90,7 +110,7 @@ sign2.style.color = "#fff"
     header.style.backgroundColor = "#fff";
     formCon.style.backgroundColor = "#fff";
     formCon.style.color ="#000"
-    signUpBtn.style.backgroundColor = "#000"
+    signBtn.style.backgroundColor = "#000"
 sign1.style.color = "#000"
 sign2.style.color = "#000"
   
